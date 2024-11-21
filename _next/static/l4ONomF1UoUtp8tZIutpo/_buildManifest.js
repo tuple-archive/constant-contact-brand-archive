28a324febac8d368ae910efa1cec949af91dfed8
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2024-11-21 12:40:11 +0000

    Nxet.js static export
    Signed-off-by: Joshua Baker <160484+joshuabaker@users.noreply.github.com>

---
+++ _next/static/l4ONomF1UoUtp8tZIutpo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F05d954cf.819eb8f31cfd3d1ff15c.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
